24f2fa12aea013731fe34345c314191deb7e946e
Pietro Borgaro <pietro.borgaro@gmail.com>
2023-02-13 11:26:02 +0100

    another fix in seed_all

--- pytorch_benchmarks/utils/utils.py
+++ pytorch_benchmarks/utils/utils.py
@@ -212,6 +212,7 @@ def seed_all(seed=None):
     if seed is not None:
         random.seed(seed)
         np.random.seed(seed)
+        torch.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
         cudnn.benchmark = False
         torch.use_deterministic_algorithms(True)
