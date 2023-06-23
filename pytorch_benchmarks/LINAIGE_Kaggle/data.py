7cb9cf01a5065a737ed6ea4af603474c12d818bf
S-M-Mollaei <mollaeiimortezaa@gmail.com>
2023-06-23 19:30:17 +0200

    update to generalized version

--- pytorch_benchmarks/LINAIGE_Kaggle/data.py
+++ pytorch_benchmarks/LINAIGE_Kaggle/data.py
@@ -4,7 +4,7 @@ from typing import Tuple
 from pathlib import Path
 import pickle
 import os
-import opendatasets as od
+import opendatasets as od 
 from sklearn.utils import class_weight
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
@@ -64,11 +64,9 @@ class Linaige_set(Dataset):
 
     def __len__(self):
         return len(self.samples)
-
-
-def dataset_cross_validation(data:pd.DataFrame, windowing:int, confindence:str, remove_frame:bool, classification:bool):
     
-    remove_frame_index = 8
+def get_session(data:pd.DataFrame, windowing:int, confindence:str, remove_frame:bool, classification:bool, session_number:int, test_split:float):
+    
     if windowing < 2: # sinle frame implementation
         
         # Checking for eleminating records with hard confidence
@@ -82,6 +80,141 @@ def dataset_cross_validation(data:pd.DataFrame, windowing:int, confindence:str, 
             data.loc[:,"target"] = (data["people_number"]>1).astype(int) 
         else:
             data.loc[:,"target"] = (data["people_number"]).astype(int)
+        
+        '''
+        # Reshaping vector(64,) of pixels to (1,8,8)
+        # Storing tuple of (images, labels) of each session in sessions_image_label
+        # Seperating data for each specific session 
+        '''
+        
+        pixel_df = data[data["session"]==session_number]
+        y_train_s1 = pixel_df.values[:, -1]
+        x_train_s1 = pixel_df.values[:,2:66].reshape((pixel_df.shape[0],1,8,8)).astype(np.float32)
+     
+        if classification:
+            class_weights = get_class_weight(y_train_s1)
+        else:
+            class_weights = get_class_weight(y_train_s1)[1] 
+        
+        if test_split != None:
+            x_train, x_test, y_train, y_test = train_test_split(x_train_s1, y_train_s1, test_size=test_split, random_state=None, shuffle=True, stratify=None)
+            return x_train, y_train, x_test, y_test, class_weights
+        else:
+            x_train = x_train_s1
+            y_train = y_train_s1
+            return x_train, y_train, 0, 0, class_weights
+
+    else: # windowing implementation
+        
+        # Changing the target to a binary classification problem
+        if classification == False:
+            data.loc[:,"target"] = (data["people_number"]>1).astype(int) 
+        else:
+            data.loc[:,"target"] = (data["people_number"]).astype(int)
+        
+        pixel_df = data[data["session"]==session_number]
+        y_train_s1 = pixel_df.values[:, -1]
+        x_train_s1 = pixel_df.values[:,2:66].reshape((pixel_df.shape[0],1,8,8)).astype(np.float32)
+        conf = pixel_df.values[:, -3]
+        
+        '''
+        # Preparing train sets
+        # First, performing "cross_validation" to have train sets back2back e.g. session 1 to 4
+        # Second, performing "windowing" (considering "confidence") by using 'concatenate' of 'window_size' images with dimension (1,8,8) to have one output as (window_size,8,8)  
+        # In case "confindence=easy", the confidence of the last frame for each window is check whether it is 'e' or not
+        # If it is 'h', we avoid to build this window and skip it to the next windowing step
+        # N.B. no need to remove first frames for train set
+        '''
+        
+        if classification:
+            class_weights = get_class_weight(y_train_s1)
+        else:
+            class_weights = get_class_weight(y_train_s1)[1] 
+            
+        # Performing 'windowing'
+        flag_easy = True if confindence == 'easy' else False
+        
+        '''################# Train Part#################'''
+        x_train_w = np.array([])
+        y_train_w_list = list()
+        
+        for i in range(len(x_train_s1) - windowing + 1): # Total number of element after windowing is len() - w + 1
+            
+            if flag_easy and conf[i + windowing - 1] == 'h': # If we want just 'easy' frames and skip 'hard' ones ???? is it needed for train part????
+                pass
+            else:
+                x_combined = x_train_s1[i, :]
+                for j in range(windowing - 1): # For e.g. w=3, we need to concatenate i, i+1, i+2, so j+1 would 1 and 2 to produce i+1 and i+2
+                    # Concatenating images (1,8,8) to get (windowing,8,8)
+                    x_combined = np.concatenate((x_combined, x_train_s1[i + j + 1, :]), axis=0)
+
+                # To convert (windowing,8,8) to (1,windowing,8,8)
+                x_combined_reshape = x_combined[np.newaxis, :]
+                # To vstack an empty array with another array (n_D) we need to check the array exists and then align the deminasion for concat to have (x_train_w.shape[0],windowing,8,8)
+                x_train_w = np.vstack([x_train_w, x_combined_reshape]) if x_train_w.size else x_combined_reshape
+                # Appending labels with size of 'windowing' as a list, then converting to numpy array
+                y_train_w_list.append(y_train_s1[(i + windowing -1)])
+
+        # To have numpy array of labels with shape (y_train_w.shape[0])
+        y_train_w = np.array(y_train_w_list)
+        
+        if test_split != None:
+            x_train, x_test, y_train, y_test = train_test_split(x_train_w, y_train_w, test_size=test_split, random_state=None, shuffle=True, stratify=None)
+            return x_train, y_train, x_test, y_test, class_weights
+        else:
+            x_train = x_train_w
+            y_train = y_train_w
+            return x_train, y_train, 0, 0, class_weights
+
+
+def create_majority(samples, label, confidence, majority_win, remove_frame, remove_frame_index):
+    X_win = []
+    
+    for i in range(len(samples) - majority_win + 1):
+        window = samples[i:i+majority_win]
+        X_win.append(window)        
+    X_win = np.array(X_win)
+    
+    # label
+    y_win = np.array(label[(majority_win - 1):])
+    conf_win = np.array(confidence[(majority_win - 1):])
+
+
+    if remove_frame and  majority_win <= remove_frame_index: # if window size is more than 8, we can not remove any thing since we need the eighth element for our first comparison
+        cutting_index = remove_frame_index - majority_win + 1 # By this index, we can reach the first element for comparison and remove previous ones
+        X_win = X_win[cutting_index:]
+        y_win = y_win[cutting_index:]
+        conf_win = conf_win[cutting_index:]
+    
+    # remove windows with hard confidence
+    e_idx = np.where(conf_win == 'e')[0]
+    X_win_e = X_win[e_idx]
+    y_win_e = y_win[e_idx]  
+    
+    return X_win_e, y_win_e
+
+        
+
+def dataset_cross_validation(data:pd.DataFrame, windowing:int, confindence:str, remove_frame:bool, classification:bool, majority_win:int):
+    
+    remove_frame_index = 8
+    if windowing < 2: # sinle frame implementation
+        
+        data_majority = data
+        
+        # Checking for eleminating records with hard confidence
+        if confindence == 'easy':
+            data = data[data["confidence"]=='e'] 
+        else:
+            pass
+        
+        # Changing the target to a binary classification problem
+        if classification == False:
+            data.loc[:,"target"] = (data["people_number"]>1).astype(int)
+            data_majority.loc[:,"target"] = (data_majority["people_number"]>1).astype(int)
+        else:
+            data.loc[:,"target"] = (data["people_number"]).astype(int)
+            data_majority.loc[:,"target"] = (data_majority["people_number"]).astype(int)
         
         # Finding how many distinct sessions exist
         session_numbers = set(data.loc[:, 'session'])
@@ -97,7 +230,15 @@ def dataset_cross_validation(data:pd.DataFrame, windowing:int, confindence:str, 
             labels = pixel_df.values[:, -1]
             images = pixel_df.values[:,2:66].reshape((pixel_df.shape[0],1,8,8)).astype(np.float32)
             sessions_image_label.append((images,labels))
-        
+            
+        sessions_image_label_majority = list()
+        for sn in session_numbers:
+            pixel_df_majority = data_majority[data_majority["session"]==sn]
+            labels_majority = pixel_df_majority.values[:, -1]
+            images_majority = pixel_df_majority.values[:,2:66].reshape((pixel_df_majority.shape[0],1,8,8)).astype(np.float32)
+            conf_majority = pixel_df_majority.values[:, -3]
+            sessions_image_label_majority.append((images_majority,labels_majority,conf_majority))   
+           
         '''
         # Starting "cross_validation" part and removing first frames
         # 'remove_frame' can be either true or false to remove first 8 frames of test session
@@ -115,8 +256,8 @@ def dataset_cross_validation(data:pd.DataFrame, windowing:int, confindence:str, 
             '''
             # Preparing train sets
             # Putting images and lebals of all sessions together,respectively.
-            # sessions_image_label[i][0] stores image session of (8,8,1) and sessions_image_label[i][1] stores corresponding labels
-            # At last, x_train includes all images of (8,8,1) from e.g. sessions 1 to 4, and y_train includes corresponding labels
+            # sessions_image_label[i][0] stores image session of (1,8,8) and sessions_image_label[i][1] stores corresponding labels
+            # At last, x_train includes all images of (1,8,8) from e.g. sessions 1 to 4, and y_train includes corresponding labels
             '''
             for i in range(len(sessions_image_label)):
                 if i != remove_seesion:   
@@ -138,22 +279,37 @@ def dataset_cross_validation(data:pd.DataFrame, windowing:int, confindence:str, 
             '''
             if remove_frame:
                 x_test = sessions_image_label[remove_seesion][0][remove_frame_index:]
-                y_test = sessions_image_label[remove_seesion][1][remove_frame_index:]   
+                y_test = sessions_image_label[remove_seesion][1][remove_frame_index:]
             else:
                 x_test = sessions_image_label[remove_seesion][0]
                 y_test = sessions_image_label[remove_seesion][1]
             
-            '''Shuffling both train and test datasets'''
+            # Majority windoing part
+            if majority_win != None:
+                majority_samples = sessions_image_label_majority[remove_seesion][0]
+                majority_labels = sessions_image_label_majority[remove_seesion][1]
+                majority_conf = sessions_image_label_majority[remove_seesion][2]
+                
+                x_test_majority, y_test_majority = create_majority(majority_samples, majority_labels, majority_conf, majority_win, remove_frame, remove_frame_index)
+
+                test_indices_majority = np.arange(x_test_majority.shape[0])
+                np.random.shuffle(test_indices_majority)
+                
+                x_test_majority = x_test_majority[test_indices_majority]
+                y_test_majority = y_test_majority[test_indices_majority]
+            else:
+                 x_test_majority, y_test_majority = ([],[])             
             
             # from sklearn.model_selection import train_test_split
-            
             # x_train, y_train, _, _ = train_test_split(x_train, y_train, test_size=0, shuffle=True)
             # x_test, y_test, _, _ = train_test_split(x_test, y_test, test_size=0, shuffle=True)
+            
             test_indices = np.arange(x_test.shape[0])
             np.random.shuffle(test_indices)
 
             x_test = x_test[test_indices]
-            y_test = y_test[test_indices]
+            y_test = y_test[test_indices]          
+
             
             train_indices = np.arange(x_train.shape[0])
             np.random.shuffle(train_indices)
@@ -161,16 +317,15 @@ def dataset_cross_validation(data:pd.DataFrame, windowing:int, confindence:str, 
             x_train = x_train[train_indices]
             y_train = y_train[train_indices]
             
-            
             pickle_dict = {'removed_session': remove_seesion,'x_train': x_train, 'y_train': y_train, 'x_test': x_test, 'y_test': y_test}
             with open('./pickle_file_single', 'wb') as f:
                  pickle.dump(pickle_dict, f, pickle.HIGHEST_PROTOCOL)
             
-            yield x_train, y_train, x_test, y_test, class_weights
+            yield x_train, y_train, x_test, y_test, x_test_majority, y_test_majority, class_weights
             remove_seesion -= 1
     
     else: # windowing implementation
-        
+           
         # Changing the target to a binary classification problem
         if classification == False:
             data.loc[:,"target"] = (data["people_number"]>1).astype(int) 
@@ -214,7 +369,7 @@ def dataset_cross_validation(data:pd.DataFrame, windowing:int, confindence:str, 
             # Preparing train sets
             # Putting images and lebals of all sessions together,respectively.
             # sessions_image_label[i][0] stores image session of (8,8,1) and sessions_image_label[i][1] stores corresponding labels sessions_image_label[i][2] stores corresponding conf
-            # At last, x_train includes all images of (8,8,1) from e.g. sessions 1 to 4, y_train includes corresponding labels and conf_train includes corresponding conf
+            # At last, x_train includes all images of (1,8,8) from e.g. sessions 1 to 4, y_train includes corresponding labels and conf_train includes corresponding conf
             
             for i in range(len(sessions_image_label_confidence)):
                 if i != remove_seesion:   
@@ -313,12 +468,12 @@ def dataset_cross_validation(data:pd.DataFrame, windowing:int, confindence:str, 
             with open('./pickle_file_window', 'wb') as f:
                 pickle.dump(pickle_dict, f, pickle.HIGHEST_PROTOCOL)
             
-            yield x_train, y_train, x_test, y_test, class_weights
+            yield x_train, y_train, x_test, y_test, [], [], class_weights
             remove_seesion -= 1
         
             
    
-def get_data(win_size:int, confindence:str, remove_frame:bool, classification:bool, data_dir:str=None) -> Tuple[Dataset, ...]:
+def get_data(win_size:int, confindence:str, remove_frame:bool, classification:bool, data_dir:str=None, session_number:int=None, test_split:float=None, majority_win:int=None) -> Tuple[Dataset, ...]:
     
     # Creating name for directory 
     if data_dir is None:
@@ -347,19 +502,47 @@ def get_data(win_size:int, confindence:str, remove_frame:bool, classification:bo
     # remove_frame: can be 'True' or 'False' to remove first 8 frames of test set for making comparison fair
     # classification: can be 'True' or 'False' to have classification or regression (binary classification)
     '''
-    dataset_cv = dataset_cross_validation(data, win_size, confindence, remove_frame, classification)
-    
-    return dataset_cv, class_number
+    # Creating dataset of session 1 (or any) to be used for warm_up and NAS search to avoid data leakage
+    if session_number != None:
+        x_train, y_train, x_test, y_test, class_weights = get_session(data, win_size, confindence, remove_frame, classification, session_number, test_split)     
+        
+        train_set = Linaige_set(x_train, y_train)
+        # Feeding the dataloader
+        train_loader = DataLoader(
+            train_set,
+            batch_size=128,
+            shuffle=True,
+            num_workers=2,
+        )
+        
+        if x_test !=0:
+            test_set = Linaige_set(x_test, y_test)
+            test_loader = DataLoader(
+                test_set,
+                batch_size=128,
+                shuffle=False,
+                num_workers=2,
+            )
+            
+            return train_loader, test_loader, x_train, class_weights, class_number
+        else:
+            return train_loader, 0, x_train, class_weights, class_number       
+       
+    # Creating datasets by cross validation to be used for fune_tuning process after NAS
+    else:
+        dataset_cv = dataset_cross_validation(data, win_size, confindence, remove_frame, classification, majority_win)
+        return dataset_cv, class_number
 
 
 def build_dataloaders(datasets: Tuple[Dataset, ...], batch_size=128, num_workers=2) -> Tuple[DataLoader, DataLoader]:
     
     # Extracting datasets from get_data function
-    x_train, y_train, x_test, y_test, class_weight = datasets
+    x_train, y_train, x_test, y_test, x_test_majority, y_test_majority, class_weights = datasets
     
 
     train_set = Linaige_set(x_train, y_train)
     test_set = Linaige_set(x_test, y_test)
+    majority_set = Linaige_set(x_test_majority, y_test_majority)
     
     # Feeding the dataloader
     train_loader = DataLoader(
@@ -376,4 +559,14 @@ def build_dataloaders(datasets: Tuple[Dataset, ...], batch_size=128, num_workers
         num_workers=num_workers,
     )
     
-    return train_loader, test_loader
+    if len(x_test_majority) == 0:
+        majority_loader = 0
+    else:
+        majority_loader = DataLoader(
+            majority_set,
+            batch_size=len(x_test_majority),
+            shuffle=False,
+            num_workers=num_workers,
+        )        
+    
+    return train_loader, test_loader, majority_loader
--- pytorch_benchmarks/LINAIGE_Kaggle/model.py
+++ pytorch_benchmarks/LINAIGE_Kaggle/model.py
@@ -4,6 +4,11 @@ import torch.nn.functional as F
 from torchsummary import summary
 from torchviz import make_dot
 from contextlib import redirect_stdout
+from plinio.methods import PIT
+from plinio.methods import PITSuperNet
+from plinio.methods.pit.nn import PITConv2d, PITLinear
+from plinio.methods.pit_supernet.nn import PITSuperNetCombiner
+from plinio.methods.pit_supernet import PITSuperNetModule
 
 # from torchvision import models
 
@@ -54,10 +59,12 @@ def build_CNN1_FC1_pc(channel:int, classification:bool, win_size:int, class_numb
         def forward(self, x):
             x = self.bn(self.conv(x))
             x = F.relu(x)
-            x = x.view(x.size(0), -1)
+            # x = x.view(x.size(0), -1)
+            x = torch.flatten(x, 1)
             x = self.fc(x)
             if self.classification:
-              x = F.softmax(x, dim=1)
+            #   x = F.softmax(x, dim=1)
+              x = x
             else:
               x = self.sigmoid(x)
             return x
@@ -118,11 +125,13 @@ def build_CNN1_FC2_pc(channel:int, classification:bool, win_size:int, class_numb
         def forward(self, x):
             x = self.bn(self.conv(x))
             x = F.relu(x)
-            x = x.view(x.size(0), -1)
+            # x = x.view(x.size(0), -1)
+            x = torch.flatten(x, 1)
             x = F.relu(self.fc1(x))
             x = self.fc2(x)
             if self.classification:
-                x = F.softmax(x, dim=1)
+                # x = F.softmax(x, dim=1)
+                x = x
             else:
                   x = self.sigmoid(x)
             return x
@@ -185,10 +194,12 @@ def build_CNN2_FC1_pc(channel:int, classification:bool, win_size:int, class_numb
             x = self.bn(self.conv(x))
             x = F.relu(x)
             x = self.pool(x)
-            x = x.view(x.size(0), -1)
+            # x = x.view(x.size(0), -1)
+            x = torch.flatten(x, 1)
             x = self.fc(x)
             if self.classification:
-                x = F.softmax(x, dim=1)
+                # x = F.softmax(x, dim=1)
+                x = x
             else:
                 x = self.sigmoid(x)
             return x
@@ -251,11 +262,13 @@ def build_CNN2_FC2_pc(channel:int, classification:bool, win_size:int, class_numb
             x = self.bn(self.conv(x))
             x = F.relu(x)
             x = self.pool(x)
-            x = x.view(x.size(0), -1)
+            # x = x.view(x.size(0), -1)
+            x = torch.flatten(x, 1)
             x = F.relu(self.fc1(x))
             x = self.fc2(x)
             if self.classification:
-                x = F.softmax(x, dim=1)
+                # x = F.softmax(x, dim=1)
+                x = x
             else:
                 x = self.sigmoid(x)
             return x
@@ -322,10 +335,12 @@ def build_CNN3_FC1_pc(channel1:int, channel2:int, classification:bool, win_size:
             x = self.pool(x)
             x = self.bn2(self.conv2(x))
             x = F.relu(x)
-            x = x.view(x.size(0), -1)
+            # x = x.view(x.size(0), -1)
+            x = torch.flatten(x, 1)
             x = self.fc(x)
             if self.classification:
-                x = F.softmax(x, dim=1)
+                # x = F.softmax(x, dim=1)
+                x = x
             else:
                   x = self.sigmoid(x)
             return x
@@ -348,7 +363,7 @@ def build_CNN3_FC1_pc(channel1:int, channel2:int, classification:bool, win_size:
     return model
 
 # -------------------------------------------CNN3+FC2-----------------------------------------------------
-def build_CNN3_FC2_pc(channel1:int, channel2:int, classification:bool, win_size:int, class_number:int):
+def build_CNN3_FC2_pc(channel1:int, channel2:int, classification:bool, win_size:int, class_number:int): 
     '''
     Build CNN architecture 3 model with 2 dense layers
     '''
@@ -393,11 +408,13 @@ def build_CNN3_FC2_pc(channel1:int, channel2:int, classification:bool, win_size:
             x = self.pool(x)
             x = self.bn2(self.conv2(x))
             x = F.relu(x)
-            x = x.view(x.size(0), -1)
+            # x = x.view(x.size(0), -1)
+            x = torch.flatten(x, 1)
             x = F.relu(self.fc1(x))
             x = self.fc2(x)
             if self.classification:
-                x = F.softmax(x, dim=1)
+                # x = F.softmax(x, dim=1)
+                x = x
             else:
                   x = self.sigmoid(x)
             return x
@@ -419,30 +436,123 @@ def build_CNN3_FC2_pc(channel1:int, channel2:int, classification:bool, win_size:
 
     return model
 
+# ***********************************************This is specific to TNC model with new PLINIO file with window = 3***********************************************
+# -------------------------------------------CNN+TCN+FC1-----------------------------------------------------
+def build_CNN_TCN_FC1_pc(channel1:int, channel2:int, classification:bool, win_size:int, class_number:int):
+    '''
+    Build CNN architecture 3 model with 2 dense layers
+    '''
+    print("CNN3_TCN_FC1!")
+    
+    tcn_out = 32
+    
+    class CNN3_TCN_FC1_pc(nn.Module):
+        def __init__(self, channel1, channel2, classification, win_size):
+            
+            #--------------------------------------------- feature extractor----------------------------------------
+            super(CNN3_TCN_FC1_pc, self).__init__()
+            # Pass (window,8,8) input to get output (channel,6,6) with kernel (3,3) -> 8-3+1=6
+            self.conv1 = nn.Conv2d(in_channels=1, out_channels=int(channel1), kernel_size=3, bias=False)
+            self.bn1 = nn.BatchNorm2d(num_features=int(channel1))
+            # If we use MaxPooling2d layer with kernel size of 2 (stride = kernel), the output shape would be half of the input shape->(channel,3,3)
+            self.pool = nn.MaxPool2d(kernel_size=2)
+            # Pass (channel1,3,3) input to get output (channel2,1,1) with kernel (3,3) -> 3-3+1=1
+            self.conv2 = nn.Conv2d(in_channels=channel1, out_channels=int(channel2), kernel_size=3, bias=False)
+            self.bn2 = nn.BatchNorm2d(num_features=int(channel2))
+            self.sigmoid = nn.Sigmoid()
+            
+            #--------------------------------------------- tcn and fc layers----------------------------------------
+            self.tcn = nn.Conv1d(in_channels=channel2, out_channels=tcn_out, kernel_size=3)
+            self.bn3 = nn.BatchNorm1d(num_features=int(tcn_out))
+
+            self.classification = classification
+            self.fc1 = nn.Linear(tcn_out, class_number if classification else 1)
+
+        
+        def forward(self, x1, x2, x3):
+            feature_list = []
+            
+            x = F.relu(self.bn1(self.conv1(x1)))
+            x = self.pool(x)
+            x = F.relu(self.bn2(self.conv2(x)))
+            out_i = torch.flatten(x, 1)
+            out_to_cat = out_i.unsqueeze(-1)
+            
+            feature_list.append(out_to_cat)
+            
+            x = F.relu(self.bn1(self.conv1(x2)))
+            x = self.pool(x)
+            x = F.relu(self.bn2(self.conv2(x)))
+            out_i = torch.flatten(x, 1)
+            out_to_cat = out_i.unsqueeze(-1)
+            
+            feature_list.append(out_to_cat)
+            
+            x = F.relu(self.bn1(self.conv1(x3)))
+            x = self.pool(x)
+            x = F.relu(self.bn2(self.conv2(x)))
+            out_i = torch.flatten(x, 1)
+            out_to_cat = out_i.unsqueeze(-1)
+            
+            feature_list.append(out_to_cat)
+            
+            feature_out = torch.cat(feature_list, dim=-1)
+            
+            x = self.bn3(self.tcn(feature_out))
+            x = F.relu(x)
+            x = torch.flatten(x, 1)
+            x = F.relu(self.fc1(x))
+            
+            
+            if self.classification:
+                x = x
+            else:
+                  x = self.sigmoid(x)
+            return x
+
+    model = CNN3_TCN_FC1_pc(channel1, channel2, classification, win_size)
+    
+    y = model(torch.zeros((win_size, 1, 8, 8)),torch.zeros((win_size, 1, 8, 8)),torch.zeros((win_size, 1, 8, 8)))
+
+    # save the model summary
+    with open('./cnn3_tcn_fc1' + str(channel1) + '_c'+ str(channel2) +'_w'+ str(win_size) +'.txt', 'w') as fi:
+        with redirect_stdout(fi):
+            summary(model, [(1, 8, 8), (1, 8, 8), (1, 8, 8)])
+            
+    print(summary(model, [(1, 8, 8), (1, 8, 8), (1, 8, 8)]))
+
+    # plot the model architecture
+    make_dot(y, params=dict(model.named_parameters())).render('cnn3_tcn_fc1_c_' + str(channel1) + '_'+ str(channel2) +'_w_'+ str(win_size), format='png')
+
+    return model
+
+
 # -------------------------------------------Model Building-----------------------------------------------------
 def get_reference_model(model_name:str, channel1:int, channel2:int, classification:bool, win_size:int, class_number:int):
     '''
     Support windowing
     Select the model to run
-    :param model_name: 'cnn1', 'cnn1_dense', 'cnn2', 'cnn2_dense', 'cnn3', 'cnn3_dense'
+    :param model_name: 'cnn1', 'cnn1_dense', 'cnn2', 'cnn2_dense', 'cnn3', 'cnn3_dense','cnn3_tcn'
     :return:
     '''
 
-    assert (model_name in ['cnn1', 'cnn1_dense', 'cnn2', 'cnn2_dense', 'cnn3', 'cnn3_dense'])
+    assert (model_name in ['c_fc', 'c_fc_fc', 'c_p_fc', 'c_p_fc_fc', 'c_p_c_fc', 'c_p_c_fc_fc','c_p_c_tcn_fc'])
 
     # assert (classification), "the problem should be classification here!"
 
-    if model_name == 'cnn1':
+    if model_name == 'c_fc':
         model = build_CNN1_FC1_pc(channel1, classification, win_size, class_number)
-    elif model_name == 'cnn1_dense':
+    elif model_name == 'c_fc_fc':
         model = build_CNN1_FC2_pc(channel1, classification, win_size, class_number)
-    elif model_name == 'cnn2':
+    elif model_name == 'c_p_fc':
         model = build_CNN2_FC1_pc(channel1, classification, win_size, class_number)
-    elif model_name == 'cnn2_dense':
+    elif model_name == 'c_p_fc_fc':
         model = build_CNN2_FC2_pc(channel1, classification, win_size, class_number)
-    elif model_name == 'cnn3':
+    elif model_name == 'c_p_c_fc':
         model = build_CNN3_FC1_pc(channel1, channel2, classification, win_size, class_number)
-    else:
+    elif model_name == 'c_p_c_fc_fc':
         model = build_CNN3_FC2_pc(channel1, channel2, classification, win_size, class_number)
+    elif model_name == 'c_p_c_tcn_fc':
+        model = build_CNN_TCN_FC1_pc(channel1, channel2, classification, win_size, class_number)
 
     return model
